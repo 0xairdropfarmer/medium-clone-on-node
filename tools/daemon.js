fd59b19eaa4bf6910f06c1b4e4aed3c2bb01e8ba
philipsz-davido <philipsz-davido>
2018-02-17 23:07:11 +0100

    Did some stuffs

--- tools/daemon.js
+++ tools/daemon.js
@@ -1,6 +1,6 @@
 const { spawn, exec } = require('child_process')
 
-const child1 = spawn('node', ['server/app.js']);
+/*const child1 = spawn('node', ['server/app.js']);
 child1.stdout.on('data', (data) => {
     console.log(`${data}`);
 });
@@ -8,4 +8,24 @@ child1.stdout.on('data', (data) => {
 const child2 = exec('npm run start');
 child2.stdout.on('data', (data) => {
     console.log(`${data}`);
-});
+});*/
+
+function main(params) {
+    let cmds = process.argv.slice(2, process.argv.length)
+    Array.prototype.forEach.call(cmds, (cmd) => {
+        Spawn()
+    })
+    console.log(cmds)
+}
+
+function Spawn(cmd, args) {
+    const child1 = spawn(cmd, [args]);
+    child1.stdout.on('data', (data) => {
+        console.log(`${data}`);
+    });
+}
+
+function cmdToArgs(params) {
+
+}
+main()
