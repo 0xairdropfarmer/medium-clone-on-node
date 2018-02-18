64474a2e66f97f8b89fbd8089d8af81dcec7b85b
philipsz-davido <philipsz-davido>
2018-02-18 14:17:24 +0100

    Fixed some bugs

--- tools/daemon.js
+++ tools/daemon.js
@@ -1,31 +1,64 @@
-const { spawn, exec } = require('child_process')
+const path = require("path")
+const fs = require("fs")
+const { spawn } = require('child_process')
 
-/*const child1 = spawn('node', ['server/app.js']);
-child1.stdout.on('data', (data) => {
-    console.log(`${data}`);
-});
-
-const child2 = exec('npm run start');
-child2.stdout.on('data', (data) => {
-    console.log(`${data}`);
-});*/
-
-function main(params) {
+function main() {
     let cmds = process.argv.slice(2, process.argv.length)
+    let terminalPath = processPlatform()
+    console.log('\x1b[36m%s\x1b[4m', 'eidon daemon started')
+    console.log('\x1b[0m')
     Array.prototype.forEach.call(cmds, (cmd) => {
-        Spawn()
+        let terminal = terminalPath[0]
+        let arg = [...terminalPath[1], cmd]
+        Spawn(terminal, arg)
     })
-    console.log(cmds)
 }
 
-function Spawn(cmd, args) {
-    const child1 = spawn(cmd, [args]);
-    child1.stdout.on('data', (data) => {
+/**
+ * @return {*} Array
+ */
+function processPlatform() {
+    let args
+    if (process.platform == 'win32') {
+        let root = "C:\\"
+        let terminal = "cmd.exe"
+        let sysPaths = ["Windows\\System\\", "WinNT\\System32\\", "Windows\\System32\\"]
+        sysPaths.forEach((p) => {
+            let file = root + p + terminal
+            if (fs.existsSync(file)) {
+                args = [file, ['/s', '/c']]
+            }
+        })
+    } else {
+        args = ["/sh/bin", ["-c"]]
+    }
+    return args
+}
+
+/**
+ * 
+ * @param {*} terminal
+ * @param {*} args 
+ */
+function Spawn(terminal, args) {
+    const child = spawn(terminal, args);
+    child.stdout.on('data', (data) => {
         console.log(`${data}`);
     });
 }
 
-function cmdToArgs(params) {
-
+/**
+ * 
+ * @param {*} cmds 
+ */
+function cmdToArgs(cmds) {
+    let cmdArgs = []
+    Array.prototype.forEach.call(cmds, (cmd) => {
+        let c = String.prototype.split.call(cmd, " ")
+        cmd = c[0]
+        let args = Array.prototype.join.call(Array.prototype.slice.call(c, 1), " ")
+        cmdArgs.push([cmd, args])
+    })
+    return cmdArgs
 }
 main()
