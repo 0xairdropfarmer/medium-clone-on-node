d80b2325fb13175bba9e912304c528f8ec4bfef3
philipsz-davido <philipsz-davido>
2018-02-26 20:08:46 +0100

    Added FollowButton

---
+++ src/components/FollowButton.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+/** renders bg white when user not follow, render green when followed */
+class FollowButton extends Component {
+    render() {
+        return ( 
+            <div>
+            </div>
+        );
+    }
+}
+
+export default FollowButton;
