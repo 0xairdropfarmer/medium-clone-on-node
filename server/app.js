b2f6a14a6c58e2b5ff3cb891b492bfb64d7bdb49
krissanawat101 <krissanawat101@gmail.com>
2020-12-19 12:22:03 +0700

    update error

--- server/app.js
+++ server/app.js
@@ -1,46 +1,44 @@
 /** require dependencies */
-const express = require("express")
-const routes = require('./routes/')
-const mongoose = require('mongoose')
-const cors = require('cors')
-const bodyParser = require('body-parser')
-const helmet = require('helmet')
-const cloudinary = require('cloudinary')
+const express = require("express");
+const routes = require("./routes/");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+const helmet = require("helmet");
+const cloudinary = require("cloudinary");
 
-const app = express()
-const router = express.Router()
-const url = process.env.MONGODB_URI || "mongodb://localhost:27017/medium"
+const app = express();
+const router = express.Router();
+const url = process.env.MONGODB_URI || "mongodb://localhost:27017/medium";
 
 /** configure cloudinary */
 cloudinary.config({
-    cloud_name: 'chidumennamdi',
-    api_key: '',
-    api_secret: ''
-})
+  cloud_name: "chidumennamdi",
+  api_key: "",
+  api_secret: ""
+});
 
 /** connect to MongoDB datastore */
 try {
-    mongoose.connect(url, {
-        //useMongoClient: true
-    })    
-} catch (error) {
-    
-}
+  mongoose.connect(url, {
+    //useMongoClient: true
+  });
+} catch (error) {}
 
-let port = 5000 || process.env.PORT
+let port = 5000 || process.env.PORT;
 
 /** set up routes {API Endpoints} */
-routes(router)
+routes(router);
 
 /** set up middlewares */
-app.use(cors())
-app.use(bodyParser.json())
-app.use(helmet())
+app.use(cors());
+app.use(bodyParser.json());
+app.use(helmet());
 //app.use('/static',express.static(path.join(__dirname,'static')))
 
-app.use('/api', router)
+app.use("/api", router);
 
 /** start server */
 app.listen(port, () => {
-    console.log(`Server started at port: ${port}`);
+  console.log(`Server started at port: ${port}`);
 });
--- src/index.js
+++ src/index.js
@@ -1,41 +1,42 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/medium.css';
-import { Provider } from 'react-redux';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./assets/medium.css";
+import { Provider } from "react-redux";
 
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route } from "react-router-dom";
 //import { ConnectedRouter } from 'react-router-redux'; ==> OLD
 import { ConnectedRouter } from "connected-react-router";
 
-import App from './App.js';
-import registerServiceWorker from './registerServiceWorker';
+import App from "./App.js";
+import registerServiceWorker from "./registerServiceWorker";
 
-import configureStore { history } from './redux/store';
+import configureStore, { history } from "./redux/store";
 
-import { getUser } from './redux/actions/actions';
+import { getUser } from "./redux/actions/actions";
 
 const store = configureStore();
 
-if(localStorage.Auth) {
-    console.log('first dispatch')
-    //console.log(localStorage.Auth)
-    // update localstorage
-    store.dispatch({type: 'SET_USER', user: JSON.parse(localStorage.Auth)})
+if (localStorage.Auth) {
+  console.log("first dispatch");
+  //console.log(localStorage.Auth)
+  // update localstorage
+  store.dispatch({ type: "SET_USER", user: JSON.parse(localStorage.Auth) });
 
-    var _id = JSON.parse(localStorage.Auth)._id
-    getUser(_id).then((res)=>{
-        //console.log(JSON.parse(res))
-        store.dispatch({type: 'SET_USER', user: res})
-    })
+  var _id = JSON.parse(localStorage.Auth)._id;
+  getUser(_id).then((res) => {
+    //console.log(JSON.parse(res))
+    store.dispatch({ type: "SET_USER", user: res });
+  });
 }
 
-ReactDOM.render((
-    <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <Switch>
-                <Route path="/" component={App} />
-            </Switch>
-        </ConnectedRouter>
-    </Provider>
-), document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <ConnectedRouter history={history}>
+      <Switch>
+        <Route path="/" component={App} />
+      </Switch>
+    </ConnectedRouter>
+  </Provider>,
+  document.getElementById("root")
+);
 registerServiceWorker();
